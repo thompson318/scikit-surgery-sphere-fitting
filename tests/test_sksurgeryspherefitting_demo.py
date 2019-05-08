3e647b772833543cf03563a52bf4f26ef139cea4
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-05-08 11:40:05 +0100

    Issue #2 added test for demo

---
+++ tests/test_sksurgeryspherefitting_demo.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+"""scikit-surgery-sphere-fitting tests"""
+
+from sksurgeryspherefitting.ui.sksurgeryspherefitting_demo import run_demo
+
+def test_fit_sphere_least_squares_demo():
+
+    model_name = 'data/CT_Level_1.vtp'
+    output_name = 'out_temp.vtp'
+    
+    run_demo ( model_name, output_name )
+
