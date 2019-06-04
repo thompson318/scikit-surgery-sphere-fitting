4b6d4ca331b4d80e3c6fc3b2882247cb81011667
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-06-04 18:19:20 +0100

    Issue #3 added tests to linting

--- tests/test_sksurgeryspherefitting_demo.py
+++ tests/test_sksurgeryspherefitting_demo.py
@@ -4,10 +4,10 @@
 
 from sksurgeryspherefitting.ui.sksurgeryspherefitting_demo import run_demo
 
-def test_fit_sphere_least_squares_demo():
+def test_fit_sphere_least_sqs_demo():
+    """ test the run demo entry point """
 
     model_name = 'data/CT_Level_1.vtp'
     output_name = 'out_temp.vtp'
-    
-    run_demo ( model_name, output_name )
 
+    run_demo(model_name, output_name)
