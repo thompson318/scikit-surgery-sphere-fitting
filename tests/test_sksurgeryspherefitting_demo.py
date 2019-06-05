0dee977212ef5e4a752e0e0693742e3258fc7aef
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-06-05 10:19:17 +0100

    Issue #3 got demo unit tests working

--- tests/test_sksurgeryspherefitting_demo.py
+++ tests/test_sksurgeryspherefitting_demo.py
@@ -11,3 +11,21 @@ def test_fit_sphere_least_sqs_demo():
     output_name = 'out_temp.vtp'
 
     run_demo(model_name, output_name)
+
+
+def test_fit_sphere_config_demo():
+    """ test the run demo entry point """
+
+    model_name = 'data/US_Sphere_2.vtp'
+    config_name = 'data/config_init_with_bounds.json'
+
+    run_demo(model_name, configfile=config_name)
+
+
+def test_fit_sphere_config_rad_demo():
+    """ test the run demo entry point """
+
+    model_name = 'data/US_Sphere_2.vtp'
+    config_name = 'data/config_fixed_radius.json'
+
+    run_demo(model_name, configfile=config_name)
